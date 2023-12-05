a9e63267ab26e4b790e1e62c2ffaba17e0274d64
Andre Pacheco <andpacheco@ebay.com>
2023-12-05 08:41:07 -0300

    added cache method

--- services/cache.js
+++ services/cache.js
@@ -7,16 +7,26 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function (options = {}) {
+	this.useCache = true;
+	this.hashKey = options.key;
+	return this;
+};
+
 mongoose.Query.prototype.exec = async function () {
+	if (!this.useCache) {
+		return exec.apply(this, arguments);
+	}
 	const key = JSON.stringify(Object.assign({}, { collection: this.mongooseCollection.name}, this.getQuery()));
 	const cacheValue = await client.get(key);
 	if (cacheValue) {
-		console.log('Retrieved from cache');
-		return JSON.parse(cacheValue);
+		const doc = JSON.parse(cacheValue);
+		console.log('cache hit');
+		return Array.isArray(doc) ? doc.map((d => new this.model(d))) : new this.model(doc);
 	}
 	const result = await exec.apply(this, arguments);
 
-	client.set(key, JSON.stringify(result));
+	client.set(key, JSON.stringify(result), 'EX', 10);
 
 	return result;
 };
