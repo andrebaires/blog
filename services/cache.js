dd15ab9eb5d3a9c049ef42dfb990472055000093
Andre Pacheco <andpacheco@ebay.com>
2024-01-11 15:54:07 -0300

    nested cache keys

--- services/cache.js
+++ services/cache.js
@@ -4,12 +4,12 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.cache = function (options = {}) {
 	this.useCache = true;
-	this.hashKey = options.key;
+	this.hashKey = JSON.stringify(options.key || '');
 	return this;
 };
 
@@ -18,7 +18,8 @@ mongoose.Query.prototype.exec = async function () {
 		return exec.apply(this, arguments);
 	}
 	const key = JSON.stringify(Object.assign({}, { collection: this.mongooseCollection.name}, this.getQuery()));
-	const cacheValue = await client.get(key);
+
+	const cacheValue = await client.hget(this.hashKey, key);
 	if (cacheValue) {
 		const doc = JSON.parse(cacheValue);
 		console.log('cache hit');
@@ -26,7 +27,13 @@ mongoose.Query.prototype.exec = async function () {
 	}
 	const result = await exec.apply(this, arguments);
 
-	client.set(key, JSON.stringify(result), 'EX', 10);
+	client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
 	return result;
+};
+
+module.exports = {
+	clearHash(hashKey) {
+		client.del(JSON.stringify(hashKey));
+	}
 };
